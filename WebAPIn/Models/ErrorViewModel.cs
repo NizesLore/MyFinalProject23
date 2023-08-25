d08ca1f761219542137ff251f084800538c52ecc
sezin sezin <fenciszn@hotmail.com>
2023-08-25 20:04:51 +0300

    .

--- WebAPIn/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebAPIn.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
